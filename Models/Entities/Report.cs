3283357d23028725925d525888fd83476ec845e4
george <badjokeresult@gmail.com>
2023-01-28 19:16:47 +0300

    report-entity-provided

---
+++ Models/Entities/Report.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace Reports.Models.Entities;
+
+public class Report
+{
+    public int Id { get; }
+    public TimeSpan? CreationTime { get; set; }
+    public bool IsSuccessful { get; set; }
+
+    public Report(int id, TimeSpan? creationTime, bool isSuccessful)
+    {
+        Id = id;
+        CreationTime = creationTime;
+        IsSuccessful = isSuccessful;
+    }
+
+    public override string ToString()
+    {
+        var builder = new StringBuilder();
+        
+        builder.AppendLine($"ID : {Id}");
+        builder.AppendLine(IsSuccessful
+            ? $"Creation Time : {CreationTime!.Value}"
+            : "Creation Time is unknown");
+        builder.AppendLine($"Is successful : {IsSuccessful}");
+
+        return builder.ToString();
+    }
+}
