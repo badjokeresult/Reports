8126699c5c30dceee164ed67787f6af253d09194
george <badjokeresult@gmail.com>
2023-01-28 19:16:18 +0300

    db-conn-provided

---
+++ Models/AppDbContext/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using Reports.Models.Entities;
+
+namespace Reports.Models.ApplicationContext;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Report> Reports { get; set; } = null!;
+ 
+    public AppDbContext()
+    {
+        Database.EnsureCreated();
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=report_project;Username=report_user;Password=r3P0rt$");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Report>(entity => entity.HasKey(r => r.Id));
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
