4e43d59a06de3a0561dfd4717b7695b53bc23273
george <badjokeresult@gmail.com>
2023-01-28 19:17:14 +0300

    db-worker-provided

---
+++ Models/Workers/DbWorker.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using Reports.Models.ApplicationContext;
+using Reports.Models.Entities;
+
+namespace Reports.Models.Workers;
+
+public class DbWorker : IDbWorker
+{
+    public async Task CreateAsync(Report report)
+    {
+        await using var db = new AppDbContext();
+
+        await db.Reports.AddAsync(report);
+
+        await db.SaveChangesAsync();
+    }
+
+    public async Task<Report?> GetAsync(int id)
+    {
+        await using var db = new AppDbContext();
+
+        var report = await db.Reports.FirstOrDefaultAsync(r => r.Id == id);
+
+        return report;
+    }
+
+    public async Task<IEnumerable<Report>> GetAllAsync()
+    {
+        await using var db = new AppDbContext();
+
+        var reports = db.Reports.AsEnumerable();
+
+        return reports;
+    }
+
+    public async Task ChangeAsync(int id, Report newReport)
+    {
+        await using var db = new AppDbContext();
+
+        var oldReport = await db.Reports.FirstOrDefaultAsync(r => r.Id == id);
+
+        if (oldReport != null)
+        {
+            oldReport.CreationTime = newReport.CreationTime;
+            oldReport.IsSuccessful = newReport.IsSuccessful;
+            
+            await db.SaveChangesAsync();
+        }
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        await using var db = new AppDbContext();
+
+        var report = db.Reports.FirstOrDefault(r => r.Id == id);
+        if (report != null)
+            db.Reports.Remove(report);
+
+        await db.SaveChangesAsync();
+    }
+}
---
+++ Models/Workers/IDbWorker.cs
@@ -0,0 +1,12 @@
+﻿using Reports.Models.Entities;
+
+namespace Reports.Models.Workers;
+
+public interface IDbWorker
+{
+    public Task CreateAsync(Report report);
+    public Task<Report?> GetAsync(int id);
+    public Task<IEnumerable<Report>> GetAllAsync();
+    public Task ChangeAsync(int id, Report newReport);
+    public Task DeleteAsync(int id);
+}
